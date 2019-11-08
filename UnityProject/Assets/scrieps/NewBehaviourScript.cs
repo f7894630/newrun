bacede655ef9592ee2e6ce5d2b3a3224e831bcde
f7894630 <56706544+f7894630@users.noreply.github.com>
2019-11-08 12:01:44 +0800

    scriptone

---
+++ UnityProject/Assets/scrieps/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("speed")][Range(0f,100f)]
+    public float speed = 3.5f;
+    [Header("jump")][Range(100,2000)]
+    public int jump = 300;
+    [Header("standornot")][Tooltip("判斷角色是否在陸地")]
+    public bool isGround = false;
+    [Header("name")]
+    public string name = "YEE";
+}
